563696f68d561955355e3fb04ccc8389f3464170
Sergey <Sergey@sergeys-mbp>
2017-05-31 09:34:13 -0700

    created "main board" and "table view" added two button on "main board" created segue from two buttons that open "table view" and send the "title of button" to "title of table view"

--- Recipe-Shopping/ViewController.swift
+++ Recipe-Shopping/ViewController.swift
@@ -9,16 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "Appetizers"{
+            
+            let detailVC = segue.destination as! MyTableViewController
+            detailVC.HeadTitle = "Appetizers"
+        }
+        else if segue.identifier == "MainDish" {
+            
+            let detailVC = segue.destination as! MyTableViewController
+            detailVC.HeadTitle = "Main Dish"
+            
+            
+           
+        }
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+
 
 
 }
