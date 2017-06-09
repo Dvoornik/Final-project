776c5457832f9573e90db44348d421301a7ec868
Sergey <Sergey@sergeys-mbp>
2017-06-08 21:53:07 -0700

    version with core date (doesn't work)

--- Recipe-Shopping/MyTableViewController.swift
+++ Recipe-Shopping/MyTableViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import CoreData
 
-class MyTableViewController: UITableViewController {
+class MyTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
     
     var HeadTitle : String!
+    var MyDish : [DishDO] = []
+    var fetchResultsController : NSFetchedResultsController<DishDO>!
     
-    var AppetizersRecipeBook = [
+    var dishes = ["Bruchetta","Fried Chicken"]
+    var type = ["Appetizers","Main Dish"]
+    var image = ["appetizers","maindish"]
+    
+    
+    /*var AppetizersRecipeBook = [
         Recipe(recType: "Appetizers", recName: "Bruchetta", recImage: #imageLiteral(resourceName: "appetizers"),
     ingredients: [
                 Ingredient(ingrType: "Bread", name: "Baguette", amount: "1 lb", ingrImage: #imageLiteral(resourceName: "appetizers")),
@@ -28,10 +36,12 @@ class MyTableViewController: UITableViewController {
                 Ingredient(ingrType: "Condiment", name: "Ketchup", amount: "50 oz", ingrImage: #imageLiteral(resourceName: "maindish")),
                 Ingredient(ingrType: "Condiment", name: "Ranch Sauce", amount: "50 oz", ingrImage: #imageLiteral(resourceName: "maindish")),
                 Ingredient(ingrType: "Vegetable", name: "Potato", amount: "1 lb", ingrImage: #imageLiteral(resourceName: "maindish"))])
-    ]
+    ]*/
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addData()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -40,6 +50,54 @@ class MyTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
         self.navigationItem.title = self.HeadTitle
+        
+        let fetchRequest : NSFetchRequest<DishDO> = DishDO.fetchRequest()
+        if let appDelegate = (UIApplication.shared.delegate as? AppDelegate) {
+            let context = appDelegate.persistentContainer.viewContext
+            fetchResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+            fetchResultsController.delegate = self
+            
+            do {
+                try fetchResultsController.performFetch()
+                if let fetchedObjects = fetchResultsController.fetchedObjects {
+                    MyDish = fetchedObjects
+            
+                }
+            }
+                catch {
+                    print(error)
+                }
+            }
+        }
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?){
+        switch type {
+            case .insert:
+                if let newIndexPath = newIndexPath {
+                tableView.insertRows(at: [newIndexPath], with: .fade)
+            }
+            case .delete:
+                if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            case .update:
+            if let indexPath = indexPath {
+                tableView.reloadRows(at: [indexPath], with: .fade)
+            }
+            default:
+            tableView.reloadData()
+        }
+        
+        if let fetchedObjects = controller.fetchedObjects {
+            MyDish = fetchedObjects as! [DishDO]
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
     }
 
     override func didReceiveMemoryWarning() {
@@ -55,12 +113,18 @@ class MyTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         var ItemCount : Int!
-        if self.HeadTitle == "Appetizers"{
-            ItemCount = AppetizersRecipeBook.count
+        var Item : DishDO
+        Item = MyDish[section]
+        if self.HeadTitle == Item.iType {
+            ItemCount = MyDish.count
         }
-        else if self.HeadTitle == "Main Dish"{
-            ItemCount = MainDishesRecipeBook.count
+        else if self.HeadTitle == Item.iType{
+            ItemCount = MyDish.count
+        }
+        else {
+            ItemCount = 1
         }
         // #warning Incomplete implementation, return the number of rows
         return ItemCount
@@ -71,14 +135,18 @@ class MyTableViewController: UITableViewController {
         
         let cellIdentifier = "RecipeCell"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        var cellItem : DishDO
+        cellItem = MyDish[indexPath.row]
         
-        if self.HeadTitle == "Appetizers"{
-            cell.textLabel?.text = AppetizersRecipeBook[indexPath.row].recName
-            cell.imageView?.image = AppetizersRecipeBook[indexPath.row].recImage
+        if self.HeadTitle == cellItem.iType
+        {
+            cell.textLabel?.text = cellItem.iName
+            cell.imageView?.image = UIImage(data: cellItem.iImage as! Data)
         }
-        else if self.HeadTitle == "Main Dish"{
-            cell.textLabel?.text = MainDishesRecipeBook[indexPath.row].recName
-            cell.imageView?.image = MainDishesRecipeBook[indexPath.row].recImage
+        else if self.HeadTitle == cellItem.iType
+        {
+            cell.textLabel?.text = cellItem.iName
+            cell.imageView?.image = UIImage(data: cellItem.iImage as! Data)
         }
 
         // Configure the cell...
@@ -131,5 +199,25 @@ class MyTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func addData() {
+        
+        var addItem : DishDO
+    
+        if let appDelegate = (UIApplication.shared.delegate as? AppDelegate){
+            
+            for i in 0...type.count - 1 {
+                
+                addItem = DishDO(context: appDelegate.persistentContainer.viewContext)
+                addItem.iName = dishes[i]
+                addItem.iImage = NSData(data:UIImagePNGRepresentation(UIImage(named: image[i])!)!)
+                addItem.iType = type[i]
+                appDelegate.saveContext()
+            }
+        }
+    }
+
+
+
 
 }
